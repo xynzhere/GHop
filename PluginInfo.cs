3f5d64bb2f7375e7fbc37a10a65d0ebc2c48541a
X <165595431+xynzhere@users.noreply.github.com>
2026-04-11 01:49:33 +0300

    GHop v1.0 Release!

---
+++ PluginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace ghop
+{
+    internal class PluginInfo
+    {
+        internal const string GUID = "com.xynz.ghop";
+        internal const string Name = "GHop";
+        internal const string Version = "1.0";
+    }
+}
